7c89fcdf4011db2d5e2924c1a9e3146778775ce8
HiroOfHyrule <38122876+HiroOfHyrule@users.noreply.github.com>
2018-04-12 02:05:58 -0600

    Update MedPASS_AdminViewEmpInfo.php

--- MedPASS_AdminViewEmpInfo.php
+++ MedPASS_AdminViewEmpInfo.php
@@ -26,7 +26,7 @@
 
     <section id="showcase">
       <div class="patientSubPage">
-        <h1>Your Practitioner Information</h1>
+        <h1>Employee Information</h1>
       </div>
     </section>
   </div>
